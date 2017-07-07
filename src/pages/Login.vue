14a88f52c514c7d2daee1e9a061ad745a9c1e062
xcodebin <xb865453422@hotmail.com>
2017-07-07 11:20:09 +0800

    123

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,14 +2,20 @@
   <div class="login">
     <VmLogin>
     </VmLogin>
+    <tables ref="tablex"></tables>
   </div>
 </template>
 <script>
   import VmLogin from '@/components/vm-login'
+//  import tables from '@/components/tables'
   export default {
     name: 'Login',
     components: {
       VmLogin
+//      tables
+    },
+    mounted () {
+      this.$refs.tablex.exportCsv()
     }
   }
 </script>
